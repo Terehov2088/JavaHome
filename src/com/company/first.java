88f02bc88bfafbd89ec514840442d444511b4263
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-08-16 11:19:35 +0300

    1

--- src/com/company/first.java
+++ src/com/company/first.java
@@ -7,15 +7,15 @@ public class first {
 
         System.out.println("Hello World");
 
-        double a = 8;
-        double b = 16;
-        double c = 11;
+        int a = 8;
+        float b = 16;
+        float c = 11;
         double z1, z2;
 
         z1 = a + b * c / 2;
         z2 = (pow(a, 2) + pow(b, 2)) / 2;
         double z3 = (a + b) / 12 * (c % 4) + b;
-        double z4 = ((a - b * c) / (a + b)) % c;
+        double z4 = ((a - b * c) / (a + b)) + floor(c);
         double z5 = abs(a - b) / pow((a + b), 3) - cos(c);
         double z6 = pow((log( 1 + c ) / -b ), 4) + abs(a);
 
