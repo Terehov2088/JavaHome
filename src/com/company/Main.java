888c38f83d9430c145c5f14459b553ed3786f1da
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-08-16 10:16:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//        int myNumber = 5;
+//        float f = (float) 4.5;  //float f = 4.5f
+//        char c = 'g';
+//        String s1 = new String("Who let the dogs out?");
+//        String s2 = "Who who who who!";
+//        String s3 = s1 + ' ' + s2;
+//        System.out.println(s3);
+//        int num = 5;
+//        String s = "I have " + num + " cookies";
+//        System.out.println(s);
+
+//        int a =4;
+//        int b = 5;
+//        if (a == b) System.out.println("Yeah!");
+//        else System.out.println("Ohhh...");
+
+
+
+//        String a = new String("Wow");
+//        String b = new String("Wow");
+//        String sameA = a;
+//
+//        boolean r1 = a == b;      // Ложь, так как a и b не один и тот же объект
+//        boolean r2 = a.equals(b); // Истина, так как a и b логически равны
+//        boolean r3 = a == sameA;  // Истина, так как a и sameA действительно один и тот же объект
+//        System.out.println(r1);
+//        System.out.println(r2);
+//        System.out.println(r3);
+
+//        int[] arr = {1, 9, 9, 5};
+//        for (int i = 0; i < arr.length; i++) {
+//            int el = arr[i];
+//            System.out.println(el);
+//        }
+    }
+}
---
+++ src/com/company/first.java
@@ -0,0 +1,35 @@
+package com.company;
+import static java.lang.Math.*;
+
+public class first {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        double a = 8;
+        double b = 16;
+        double c = 11;
+        double z1, z2;
+
+        z1 = a + b * c / 2;
+        z2 = (pow(a, 2) + pow(b, 2)) / 2;
+        double z3 = (a + b) / 12 * (c % 4) + b;
+        double z4 = ((a - b * c) / (a + b)) % c;
+        double z5 = abs(a - b) / pow((a + b), 3) - cos(c);
+        double z6 = pow((log( 1 + c ) / -b ), 4) + abs(a);
+
+
+
+        System.out.println(z1);
+        System.out.println(z2);
+        System.out.println(z3);
+        System.out.println(z4);
+        System.out.println(z5);
+        System.out.println(z6);
+
+
+
+
+    }
+}
